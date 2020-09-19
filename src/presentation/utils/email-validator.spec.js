e4eb894c259dab59b52a1a068808541f4c54dde7
Ítalo Lima <italojonas@hotmail.com>
2020-09-19 16:18:51 -0300

    ensure EmailValidator jest

---
+++ src/presentation/utils/email-validator.spec.js
@@ -0,0 +1,24 @@
+const validator = require('validator')
+
+class EmailValidator {
+  isValid (email) {
+    return validator.isEmail(email)
+  }
+}
+
+describe('Emal=il Validator', () => {
+  test('should return true if validator returns true', () => {
+    const sut = new EmailValidator()
+    const isEmailValid = sut.isValid('valid_email@email.com')
+
+    expect(isEmailValid).toBe(true)
+  })
+
+  test('should return false if validator returns false', () => {
+    validator.isEmailValid = false
+    const sut = new EmailValidator()
+    const isEmailValid = sut.isValid('invalid_email')
+
+    expect(isEmailValid).toBe(false)
+  })
+})
