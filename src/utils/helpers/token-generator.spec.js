1c68fa4a317908f39e491965e0488a5ce863738e
Ítalo Lima <italojonas@hotmail.com>
2020-09-21 14:33:42 -0300

    ensure TokenGenerator throws if no values provided

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,13 +1,9 @@
 const jwt = require('jsonwebtoken')
-
-class TokenGenerator {
-  async generate (id) {
-    return jwt.sign(id, 'secret_key')
-  }
-}
+const TokenGenerator = require('./token-generator')
+const MissingParamError = require('../errors/missing-param-error')
 
 const makeSut = () => {
-  return new TokenGenerator()
+  return new TokenGenerator('secret')
 }
 
 describe('', () => {
@@ -22,5 +18,24 @@ describe('', () => {
     const sut = makeSut()
     const token = await sut.generate('any_id')
     expect(token).toBe(jwt.token)
+  })
+
+  test('Should call JWT with correct values', async () => {
+    const sut = makeSut()
+    await sut.generate('any_id')
+    expect(jwt.id).toBe('any_id')
+    expect(jwt.secret).toBe(sut.secret)
+  })
+
+  test('Should throw if no secret is provided', async () => {
+    const sut = new TokenGenerator()
+    const promise = sut.generate('any_id')
+    expect(promise).rejects.toThrow(new MissingParamError('secret'))
+  })
+
+  test('Should throw if no id is provided', async () => {
+    const sut = makeSut()
+    const promise = sut.generate()
+    expect(promise).rejects.toThrow(new MissingParamError('id'))
   })
 })
