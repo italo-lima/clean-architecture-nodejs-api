0f462f2bd0bb74d83e5de5a961ec8777650585e3
Ítalo Lima <italojonas@hotmail.com>
2020-09-21 14:21:38 -0300

    ensure TokenGenerator generate token

---
+++ src/utils/helpers/token-generator.spec.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken')
+
+class TokenGenerator {
+  async generate (id) {
+    return jwt.sign(id, 'secret_key')
+  }
+}
+
+const makeSut = () => {
+  return new TokenGenerator()
+}
+
+describe('', () => {
+  test('Should return null if JWT returns null', async () => {
+    const sut = makeSut()
+    jwt.token = null
+    const token = await sut.generate('any_id')
+    expect(token).toBeNull()
+  })
+
+  test('Should return a token if JWT returns token', async () => {
+    const sut = makeSut()
+    const token = await sut.generate('any_id')
+    expect(token).toBe(jwt.token)
+  })
+})
